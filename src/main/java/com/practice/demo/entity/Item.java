7b29c94bd13f4f003484eb370a2adce695eadae5
tcckn <01verum.gard@gmail.com>
2018-11-06 19:40:23 +0900

    商品登録機能を追加

---
+++ src/main/java/com/practice/demo/entity/Item.java
@@ -0,0 +1,58 @@
+package com.practice.demo.entity;
+
+
+import javax.persistence.*;
+
+/**
+ * 商品エンティティ。
+ */
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(name = "warehouse", nullable = false)
+    private String warehouse;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(String warehouse) {
+        this.warehouse = warehouse;
+    }
+}
