c18bb042eeca291f1978ab15a6ed3fb0a9424d4a
shotgw <shosuke.tsugawa@accenture.com>
2019-02-25 16:27:56 +0900

    HelloWorld Activityの追加

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -2,6 +2,8 @@ package HelloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
+        // Lesson Java基礎①（標準出力）
         // Hello World 出力
         System.out.println("Hello World!");
     }
