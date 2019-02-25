226e3c5847e99f6826158966f30ba0c0c91d7b93
shotgw <shosuke.tsugawa@accenture.com>
2019-02-25 15:41:23 +0900

    条件分岐のActivityの追加

--- src/ConditionalBranch/ConditionalBranch.java
+++ src/ConditionalBranch/ConditionalBranch.java
@@ -2,25 +2,42 @@ package ConditionalBranch;
 
 public class ConditionalBranch {
     public static void main(String[] args) {
-        // うるう年なのかを判定してみよう！
+
+        // Lesson 条件分岐
+        // 【問題】2019/2020年のうるう年の判定をしてください。
+        // 出力結果：
+        // 2019年は、うるう年ではありません。
+        // 2020年は、うるう年です。
+
+
+        //Answer if文
         int year;
-        // うるう年の場合
         year = 2019;
-        // 結果表示の処理
         if (year % 4 == 0) {
             System.out.println(year + "年は、うるう年です！");
         }
         else {
             System.out.println(year + "年は、うるう年ではありません！");
         }
-        // うるう年でない場合
         year = 2020;
-        // 結果表示の処理
         if (year % 4 == 0) {
             System.out.println(year + "年は、うるう年です！");
         }
         else {
             System.out.println(year + "年は、うるう年ではありません！");
         }
+
+        //Answer  switch文
+        year = 2019;
+        // year = 2020;
+        switch (year % 4){
+            case 0 :
+                System.out.println(year + "年は、うるう年です。");
+                break;
+            default:
+                System.out.println(year + "年は、うるう年ではありません。");
+        }
+
+
     }
 }
