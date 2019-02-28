1f92e4583bc8b0735eae1912df9f50cc73e42971
shotgw <shosuke.tsugawa@accenture.com>
2019-02-28 16:05:02 +0900

    Java基礎①（標準出力）問題

--- src/java_basic_01/HelloWorld.java
+++ src/java_basic_01/HelloWorld.java
@@ -1,10 +1,7 @@
 package java_basic_01;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    // Lesson Java基礎①（標準出力）
+    // Hello World 出力
 
-        // Lesson Java基礎①（標準出力）
-        // Hello World 出力
-        System.out.println("Hello World!");
-    }
 }
