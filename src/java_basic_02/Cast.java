f9a72abad6f83951f7b7a49dc468e6c86ec5eaa3
shotgw <shosuke.tsugawa@accenture.com>
2019-03-05 09:33:30 +0900

    Java基礎②（変数/型/キャスト）

--- src/java_basic_02/Cast.java
+++ src/java_basic_02/Cast.java
@@ -5,7 +5,8 @@ public class Cast {
         // Lesson Java基礎②（変数/型/キャスト）
         // 小数から整数へキャスト
         double x = 10.12345;
-        System.out.println((int)x);
+        // xが整数型の10になるように出力してください
+        System.out.println();
     }
 
 }
