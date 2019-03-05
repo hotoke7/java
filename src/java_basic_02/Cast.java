0c0375c6109f2a8f81d7273db43da9e83042e21e
shotgw <shosuke.tsugawa@accenture.com>
2019-03-05 09:51:58 +0900

    Java基礎②（変数/型/キャスト）解答修正

--- src/java_basic_02/Cast.java
+++ src/java_basic_02/Cast.java
@@ -4,8 +4,9 @@ public class Cast {
     public static void main(String[] args) {
         // Lesson Java基礎②（変数/型/キャスト）
         // 小数から整数へキャスト
+        // ■ 小数点型の変数「x」を宣言して、10.12345を代入してください。
         double x = 10.12345;
-        // xが整数型の10になるように出力してください
+        // ■ xを整数型にキャストし、処理結果を出力してください。
         System.out.println((int)x);
     }
 }
