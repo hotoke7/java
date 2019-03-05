3982e440880a68c0c87399147440a20dc741ad8e
shotgw <shosuke.tsugawa@accenture.com>
2019-03-05 09:31:56 +0900

    Java基礎②（変数/型/キャスト）

--- src/java_basic_02/Cast.java
+++ src/java_basic_02/Cast.java
@@ -2,13 +2,10 @@ package java_basic_02;
 
 public class Cast {
     public static void main(String[] args) {
-        //小数から整数へキャスト
-        System.out.println((int)10.8);
-        //演算結果をキャスト(小数点切捨て)
-        System.out.println((int)(13800 * 1.08));
-        //数値をchar型へキャスト
-        int a = 97;
-        System.out.println((char)a);
+        // Lesson Java基礎②（変数/型/キャスト）
+        // 小数から整数へキャスト
+        double x = 10.12345;
+        System.out.println((int)x);
     }
 
 }
