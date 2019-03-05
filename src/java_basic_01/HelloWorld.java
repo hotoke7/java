3e32d215a30829d7509921f8b92dbbef93c94110
shotgw <shosuke.tsugawa@accenture.com>
2019-03-05 09:36:45 +0900

    Java基礎②（変数/型/キャスト）解答

--- src/java_basic_01/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package java_basic_01;
-
-public class HelloWorld {
-    // Lesson Java基礎①（標準出力）
-    // Hello World 出力
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
