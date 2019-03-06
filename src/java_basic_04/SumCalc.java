2d6d8e0790308ab1b9b740f2dfef50ceb64cc88f
shotgw <shosuke.tsugawa@accenture.com>
2019-03-06 13:34:35 +0900

    Java基礎④（繰り返し）

--- src/java_basic_04/SumCalc.java
+++ src/java_basic_04/SumCalc.java
@@ -17,6 +17,8 @@ public class SumCalc {
         // ■ 繰り返し処理を記述してください。
         for(int i=1;i<=100;i++){
             result += i;
+            // 以下の記述でも可
+            // result = result + i;
         }
         // ■ 結果を標準出力してください。
         System.out.println("1から100までの合計は、" + result + "です。");
