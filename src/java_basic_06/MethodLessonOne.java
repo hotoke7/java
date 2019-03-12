a7ab012ba85c763e7f7ff1b722e51bd47c5e55df
geniuskwon <youngjae.kwon@accenture.com>
2019-03-12 17:02:10 +0900

    Java基礎⑥（メソッド）Activity（レビュー修正３）

--- src/java_basic_06/MethodLessonOne.java
+++ src/java_basic_06/MethodLessonOne.java
@@ -3,33 +3,44 @@ package java_basic_06;
 import java.util.Scanner;
 
 public class MethodLessonOne {
+    // Lesson Java基礎⑥（メソッド）
+    // 【問題１】数字を入力すると、その数字が素数かどうかを判別する機能を作成してください
+
+    // 以下の手順に従って作成
+    // 1．メソッド作成
+    // 2. メソッド呼び出し
+
     public static void main(String[] args) {
-        // Lesson Java基礎⑥（メソッド）
-        // 【問題１】数字を入力すると、その数字が素数かどうかを判別する機能を作成してください
 
         // 標準入力
+        // 入力された数字は、変数「number」に収納されます
         Scanner sc = new Scanner(System.in);
         System.out.print("数字を入力してください。\n："); //「\n」は改行文字です。
-        int number = sc.nextInt(); // 入力された数字は、変数「number」に収納されます
+        int number = sc.nextInt();
 
+        // 2．メソッドの呼び出し
         // ■ 作成したメソッドを呼び出してください
         isPrimeNumber(number);
     }
 
+    // 1．メソッド作成
     // ■ 以下の動作をするメソッドを作成してください
-    // 引数として数字をもらい、数字が素数かどうかを判別して標準出力する
+    // 引数：数字
+    // 動作：数字が素数かどうかを判別して標準出力する
+    // 返却値：なし
     // 出力結果：
     //　 "「number」は素数です。"
     //　 "「number」は素数ではありません。"
     public static void isPrimeNumber(int number) {
-        boolean isPrime = true; // 総数可否を判断する為、booleanで変数を宣言します
+        // 素数判定を収納する変数を宣言します
+        boolean isPrimeNumber = true;
 
-        for (int i = 2; i < number; i++) { // 2から「number-1」まで繰り返します。
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-                isPrime = false; // numberをiで割ったあまりが0だったら、numberは素数ではありません
+                isPrimeNumber = false;
             }
         }
-        if (isPrime) {
+        if (isPrimeNumber) {
             System.out.println(number + "は素数です。");
         } else {
             System.out.println(number + "は素数ではありません。");
--- src/java_basic_06/MethodLessonTwo.java
+++ src/java_basic_06/MethodLessonTwo.java
@@ -3,55 +3,59 @@ package java_basic_06;
 import java.util.Scanner;
 
 public class MethodLessonTwo {
-    public static void main(String[] args) {
-        // Lesson Java基礎⑥（メソッド）
-        // 【問題２】以下の動作をする機能を作成してください
-        // ・数字を入力すると、1から入力した数字までのすべての数字を出力する
-        // ・「3または5の倍数」の場合、数字の代わりに"Rakuten!"を出力する
-        // 出力結果：
-        //   1
-        //   2
-        //   Rakuten!
-        //   4
-        // 　Rakuten!
-        // 　...
+    // Lesson Java基礎⑥（メソッド）
+    // 【問題２】以下の動作をする機能を作成してください
+    // ・数字を入力すると、1から入力した数字までのすべての数字を出力する
+    // ・「3または5の倍数」の場合、数字の代わりに"Rakuten!"を出力する
 
+    // 以下の手順に従って作成
+    // 1．メソッド①作成
+    // 2. メソッド②作成
+    // 3. 機能作成
+
+    public static void main(String[] args) {
         // 標準入力
+        // 入力された数字は、変数「number」に収納されます
         Scanner sc = new Scanner(System.in);
         System.out.print("数字を入力してください。\n："); //「\n」は改行文字です。
-        int number = sc.nextInt(); // 入力された数字は、変数「number」に収納されます
+        int number = sc.nextInt();
 
+        // 3．機能作成
         // ■ 作成したメソッド１、２を利用し、問題２の機能を作成してください
-        for (int i = 1; i <= number; i++){ // 1から入力値までを繰り返します
-            boolean checkMultipleRes = checkMultiple(i); // メソッド１呼び出し
-            String checkRakutenRes = checkRakuten(i, checkMultipleRes); // メソッド２呼び出し
-            System.out.println(checkRakutenRes); // 結果出力
+        for (int i = 1; i <= number; i++){
+            // メソッド①・②を呼び出し、結果をconvertResultに収納
+            String convertResult = convertRakuten(i, isMultiple(i));
+            System.out.println(convertResult); // 結果出力
+
+            // 一行でも書けます
+            // System.out.println(convertRakuten(i, isMultiple(i)));
         }
     }
 
-    // ■ メソッド１
-    // 以下の動作をするメソッドを作成してください
-    // 引数として数字をもらい、数字が「3または5の倍数」ならTRUEを、そうではない場合はFALSEを返却する
-    // 返却値：true　または　false
-    public static boolean checkMultiple(int num){
-        if (num % 3 == 0) {
+    // 1．メソッド①作成
+    // ■ 以下の動作をするメソッドを作成してください
+    // 引数：数字
+    // 動作：数字が「3または5の倍数」ならTRUEを、そうではない場合はFALSEを返却する
+    // 返却値：真偽値
+    public static boolean isMultiple(int num){
+        if (num % 3 == 0 || num % 5 == 0) {
             return true;
-        } else if (num % 5 == 0) {
-            return true;
-        } else {
-            return false;
         }
+        return false;
+
+        // 以下も可能です
+        // return num % 3 == 0 || num % 5 == 0;
     }
 
-    // ■ メソッド２
-    // 以下の動作をするメソッドを作成してください
-    // 引数として数字と真偽値をもらい、真偽値が「true」だったら「RAKUTEN!」を、そうではない場合は数字を返却する
+    // 2．メソッド②作成
+    // ■ 以下の動作をするメソッドを作成してください
+    // 引数：数字、真偽値
+    // 動作：真偽値が「true」だったら「RAKUTEN!」を、そうではない場合は数字を返却
     // 返却値："Rakuten!"　または　引数の数字
-    public static String checkRakuten(int num, boolean res){
-        if (res) {
+    public static String convertRakuten(int num, boolean isRakuten){
+        if (isRakuten) {
             return "Rakuten!";
-        } else {
-            return String.valueOf(num);
         }
+        return String.valueOf(num);
     }
 }
