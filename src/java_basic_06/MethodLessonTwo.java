80cf83f07f91fec3f7ece5473430d31dd7a431ff
geniuskwon <youngjae.kwon@accenture.com>
2019-03-12 11:22:01 +0900

    Java基礎⑥（メソッド）Activity（修正）

--- src/java_basic_06/MethodLessonTwo.java
+++ src/java_basic_06/MethodLessonTwo.java
@@ -5,71 +5,54 @@ import java.util.Scanner;
 public class MethodLessonTwo {
     public static void main(String[] args) {
         // Lesson Java基礎⑥（メソッド）
-        // Mainクラスからメソッドを呼び出す
-        // 引数を複数指定
-        // 呼び出されるメソッドは、戻り値があるものとないものを指定
-        // メソッド呼んで、戻り値を受けて、その値をまた別のメソッドにわたすようなものにする
-
-        // 【問題２】数字を入力して引数として渡すと、以下の動作をするメソッドを作成してください。
-        //　・1から数字までのすべての数を表示する。
-        //　・数字が3の倍数なら｢Fizz｣、5の倍数なら｢Buzz｣、両方の倍数なら｢FizzBuzz｣を数字の代わりに出力する。
-        //　・｢Fizz｣、｢Buzz｣、｢FizzBuzz｣が全部で何回出力されたかを表示する。
+        // 【問題２】以下の動作をするメソッドを作成してください。
+        //　・メソッド１
+        // 　　：数字を引数でもらい、数字が3の倍数、5の倍数ならTRUEを、そうではない場合はFALSEを返却する。
+        //　・メソッド２
+        // 　　：数字とメソッド１の結果を引数でもらい、メソッド１の結果がTRUEだったら「RAKUTEN!」を、そうではない場合は数字を返却する。
 
         Scanner sc = new Scanner(System.in);
         System.out.print("数字を入力してください。\n：");
         int number = sc.nextInt();
-        int countFizzBuzz = 0;
 
         // 1から入力値までを繰り返します。
         for (int i = 1; i <= number; i++){
-            // ■ メソッド１を作成してください。
-            String fbResult = toFizzBuzz(i);
-            System.out.println(fbResult);
+            // ■ メソッド１を呼び出してください。
+            boolean checkMultipleRes = checkMultiple(i);
 
-            // ■ メソッド２を作成してください。
-            countFizzBuzz += addCountFizzBuzz(fbResult);
+            // ■ メソッド２を呼び出してください。
+            String checkRakutenRes = checkRakuten(i, checkMultipleRes);
+
+            // ■ メソッド２の結果を出力してください。
+            System.out.println(checkRakutenRes);
         }
-
-        // ｢Fizz｣、｢Buzz｣、｢FizzBuzz｣の総件数を出力してください。
-        System.out.println("｢Fizz｣、｢Buzz｣、｢FizzBuzz｣の総件数：" + countFizzBuzz + "件");
     }
 
     // ■ メソッド１：引数をもらい、値を返却するメソッド
-    // 引数が3の倍数なら｢Fizz｣、5の倍数なら｢Buzz｣、両方の倍数なら｢FizzBuzz｣を数字の代わりに出力するメソッドを作成してください。
-    // 引数が｢Fizz｣｢Buzz｣｢FizzBuzz｣のどちらでもない場合は、数字をそのまま返却してください。
-    // Hint①：MethodLessonOneで使った「%」を利用します。
-    // Hint➁：このメソッドの返却値タイプはStringです。数字を返却するにはintをStringにキャストする必要があります。
+    // 数字を引数でもらい、数字が3の倍数、5の倍数ならTRUEを、そうではない場合はFALSEを返却する。
     // このメソッドは以下の値を返却します。
-    // 返却値：
-    // "Fizz"
-    // "Buzz"
-    // "FizzBuzz"
-    // "targetNum（引数）"
-    public static String toFizzBuzz(int targetNum){
-       String result = new String();
-        if (targetNum % 15 == 0) {
-            result = "FizzBuzz";
-        } else if (targetNum % 3 == 0) {
-            result = "Fizz";
-        } else if (targetNum % 5 == 0) {
-            result = "Buzz";
+    // 返却値：TRUE、FALSE
+    // Hint：MethodLessonOneで使った「%」を利用します。
+    public static boolean checkMultiple(int num){
+        if (num % 3 == 0) {
+            return true;
+        } else if (num % 5 == 0) {
+            return true;
         } else {
-            result = Integer.toString(targetNum);
+            return false;
         }
-        return result;
     }
 
     // ■ メソッド２：引数をもらい、値を返却するメソッド
-    // メソッド１の返却値が｢Fizz｣、｢Buzz｣、｢FizzBuzz｣の場合、1を返却するメソッドを作成してください。
-    // 引数が｢Fizz｣｢Buzz｣｢FizzBuzz｣のどちらでもない場合は、0を返却してください。
-    // 返却値：
-    // 0
-    // 1
-    public static int addCountFizzBuzz(String fizzBuzzResult){
-        if (fizzBuzzResult == "Fizz" || fizzBuzzResult == "Buzz" || fizzBuzzResult == "FizzBuzz") {
-            return 1;
+    // 数字とメソッド１の結果を引数でもらい、メソッド１の結果がTRUEだったら「RAKUTEN!」を、そうではない場合は数字を返却する。
+    // このメソッドは以下の値を返却します。
+    // 返却値："Rakuten!"、引数の数字
+    // Hint : 返却値はStringなので、引数の数字を返却するにはキャストが必要です。
+    public static String checkRakuten(int num, boolean res){
+        if (res) {
+            return "Rakuten!";
         } else {
-            return 0;
+            return String.valueOf(num);
         }
     }
 }
