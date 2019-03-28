b7b9badb5f29dc4de1ddbdc5a804cc4cd8b940a2
geniuskwon <youngjae.kwon@accenture.com>
2019-03-28 18:38:20 +0900

    Java応用②（デバッグ）Change comments

--- src/java_advance_01/ExceptionLesson.java
+++ src/java_advance_01/ExceptionLesson.java
@@ -9,12 +9,12 @@ public class ExceptionLesson {
         boolean isRegularTime;
 
         // Java応用①（例外処理）
-        // 【問題】WorkingHourRuleクラスのメソッド「isRegularTime」を使ってみましょう。
-        // ■ WorkingHourRuleクラスのメソッド「checkRegularTime」を呼び出してください。
+        // 【問題】勤務時間を入力すると定時帰りかをチェックする機能を作成してください。
+        // ■ WorkingHourRuleクラスの「checkRegularTime」メソッドを呼び出してください。
         // ・引数として、標準入力を格納した変数「workingHours」を渡してください。
         // ・返却値がtrueの場合、「定時帰りです。」を標準出力してください。
 
-        // ■ 例外処理を行ってください。
+        // ■ 「checkRegularTime」メソッドによって発生する例外に対して、例外処理を行ってください。
         // ・例外処理は「OvertimeException」、「EarlyLeaveException」の各例外別で行ってください。
         // ・例外時は、該当例外の内容を表示してください。
         // ・各例外はメッセージを持っています。例外からメッセージを出力し、以下の文字列と合わせて標準出力してください。
