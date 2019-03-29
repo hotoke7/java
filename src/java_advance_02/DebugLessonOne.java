7952b63f9fb57e925e286fe2e8572e56674788de
toshihiro.satomi <t.looklook.sato.0921@gmail.com>
2019-03-29 10:38:14 +0900

    代理修正

--- src/java_advance_02/DebugLessonOne.java
+++ src/java_advance_02/DebugLessonOne.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DebugLessonOne {
+
     // Lesson Java応用②（デバッグ）
     // ■ 以下のソースコードに潜んでいるバグを修正してください。
+    /**
+     * 文字列の内容に応じて、標準出力処理を行う。
+     *
+     * @param args -
+     */
     public static void main(String[] args) {
         List<String> dummyStringList = new ArrayList<>();
         dummyStringList.add("LIST_01");
@@ -14,26 +20,29 @@ public class DebugLessonOne {
         dummyStringList.add("LIST_04");
         dummyStringList.add("LlST_05");
 
+        // 出力想定：
+        // 01
+        // 02
+        // 不正データ
+        // 04
+        // 不正データ
         stringCutter(dummyStringList);
     }
 
-    // 文字列のリストを引数として受け取り、文字列の値によって以下の動作を行う。
-    // ・文字列の先頭4桁が「LIST」の場合、5桁目以後の文字列を標準出力
-    // ・文字列の先頭4桁が「LIST」ではない場合、「正しくないデータ」を標準出力
-    // 引数：文字列のリスト
-    // 返却値：なし
-    // 予想結果：
-    // 01
-    // 02
-    // 正しくないデータ
-    // 04
-    // 正しくないデータ
+    /**
+     * 引数の文字列リストの内容に応じて、標準出力処理を行う。
+     *
+     * ・文字列の先頭4桁が「LIST」の場合、5桁目以後の文字列を標準出力
+     * ・文字列の先頭4桁が「LIST」ではない場合、「不正データ」を標準出力
+     *
+     * @param stringList 処理対象の文字列リスト
+     */
     public static void stringCutter(List<String> stringList) {
         for (String str : stringList) {
             if (str.substring(0, 4).equals("LIST")) {
                 System.out.println(str.substring(5));
             } else {
-                System.out.println("正しくないデータ");
+                System.out.println("不正データ");
             }
         }
     }
--- src/java_advance_02/DebugLessonTwo.java
+++ src/java_advance_02/DebugLessonTwo.java
@@ -1,47 +1,62 @@
 package java_advance_02;
 
 public class DebugLessonTwo {
+
     // Lesson Java応用②（デバッグ）
     // ■ 以下のソースコードに潜んでいるバグを修正してください。
-
-    // 機能詳細
-    // 2～1000の中で「自分以外のすべての約数を足すと自分になる整数」を探し、該当整数と個数を出力する機能。
-    // 例：整数6の約数は1，2，3，6。6以外の約数を全部足すと、1 + 2 + 3 = 6になる。
-    // 予想結果：
-    // 条件を果たす数字は
-    // 6
-    // 28
-    // 496
-    // 3個です。
+    /**
+     * 2～1000の中で条件にマッチする数と、個数を出力する。
+     * <p>
+     * 条件：その数を除く全ての約数の合計が、その数自身となること
+     * 例：整数6の約数は1，2，3，6。6以外の約数を全部足すと、1 + 2 + 3 = 6のため、マッチ。
+     *
+     * @param args -
+     */
     public static void main(String[] args) {
         int count = 0;
-        int sum;
-        int remain;
-
         System.out.println("条件を果たす数字は");
 
-        // 2から1000までの整数を繰り返す。
+        // 予想結果：
+        // 条件を果たす数字は
+        // 6
+        // 28
+        // 496
+        // 3個です。
         for (int number = 2; number <= 1000; number++) {
-            sum = 0;
-
-            // factor：numberの約数の候補となる整数
-            // number自身を含まない整数を繰り返す。
-            for (int factor = 1; factor < number; factor++) {
-
-                // 整数を約数候補で割った余りが0の場合、factorをnumberの約数と判定する。
-                remain = number % factor;
-                if (remain == 0) {
-                    sum += factor;
-                }
-            }
-
-            // 約数候補の繰り返しが終了したところで、整数と約数の合計を比較する。
-            // 整数と約数合計が同一の場合、求めた数だと判定する。
-            if (number == sum) {
+            if (judgeCondition(number)) {
                 System.out.println(number);
                 count++;
             }
         }
         System.out.println(count + "個です。");
+    }
+
+    /**
+     * 引数は条件にマッチするか判定し、結果を返却する。
+     * <p>
+     * 条件１：引数の約数の個数の合計が、その数となること
+     * 条件２：引数の約数自身は約数としてカウントしないこと
+     *
+     * @param target 判定対象の数
+     * @return true:マッチ false:アンマッチ
+     */
+    private static boolean judgeCondition(int target) {
+        int remain = 0;
+        int sum = 0;
+
+        // target自身を含まない整数を繰り返す
+        for (int i = 1; i < target; i++) {
+            // 剰余算の結果が0の場合、約数
+            remain = target % i;
+            if (remain == 0) {
+                sum += i;
+            }
+        }
+
+        // 対象と約数の合計が同じ場合、条件にマッチ
+        if (target == sum) {
+            return true;
+        }
+        return false;
     }
 }
