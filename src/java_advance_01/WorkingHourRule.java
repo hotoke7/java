e605e6c8c3e276d6070d516a078cd1f1b89b82dd
toshihiro.satomi <t.looklook.sato.0921@gmail.com>
2019-03-29 17:09:10 +0900

    代理修正

--- src/java_advance_01/WorkingHourRule.java
+++ src/java_advance_01/WorkingHourRule.java
@@ -3,14 +3,16 @@ package java_advance_01;
 public class WorkingHourRule {
 
     /**
-     * 引数として渡された勤務時間の値によって、以下の処理を行う。
-     * - 勤務時間が8時間の場合：「定時帰りです。」を標準出力
-     * - 勤務時間が8時間超過の場合：OverTimeExceptionをメッセージと共に送出
-     * - 勤務時間が8時間未満の場合：EarlyLeaveExceptionをメッセージと共に送出
+     * 引数の勤務時間が、定時の勤務時間かをチェックする。
      *
-     * @param workingHours 勤務悲観（整数）
-     * @throws OvertimeException   勤務時間が正規勤務時間より大きくて、オーバータイムが発生した場合
-     * @throws EarlyLeaveException 勤務時間が正規勤務時間より少なくて、早退が発生した場合
+     * チェックの仕様は以下の通り。
+     * - 勤務時間が8時間の場合：処理終了
+     * - 勤務時間が8時間超過の場合：OverTimeExceptionを送出
+     * - 勤務時間が8時間未満の場合：EarlyLeaveExceptionを送出
+     *
+     * @param workingHours 勤務時間
+     * @throws OvertimeException   オーバータイムが発生した場合
+     * @throws EarlyLeaveException 早退が発生した場合
      */
     public void checkRegularTime(int workingHours) throws OvertimeException, EarlyLeaveException {
         int officeHours = 8;
