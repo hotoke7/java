77ae2e980296a5e61749343db8ba2162b65145f1
geniuskwon <youngjae.kwon@accenture.com>
2019-03-29 16:34:22 +0900

    Java応用②（デバッグ）Update Comments

--- src/java_advance_01/ExceptionLesson.java
+++ src/java_advance_01/ExceptionLesson.java
@@ -6,21 +6,21 @@ public class ExceptionLesson {
         int[] workingHoursList = {6, 8, 11};
 
         // Java応用①（例外処理）
-        // 【問題】勤務時間を入力すると定時帰りかをチェックする機能を作成してください。
-        // 機能の実行テストには、上記の「workingHoursList」を使用します。
-        // 「workingHoursList」の長さ分繰り返しを行い、その結果を確認します。
-        // 出力結果はWikiを参照してください。
+        // 【問題】勤務時間を入力すると定時帰りかをチェックする機能があります。
+        //         こちらの機能に上記の「workingHoursList」を渡し、正しい動作を行うように実装してください。
+        //         出力結果はWikiを参照してください。
         for (int workingHours : workingHoursList) {
             System.out.println(workingHours + "時間勤務の場合：");
 
-            // ■ WorkingHourRuleクラスの「checkRegularTime」メソッドを呼び出してください。
-            // ・引数として、変数「workingHours」を渡してください。
-            // ・「checkRegularTime」メソッドを呼び出した後に、「定時帰りです。」を標準出力してください。
-
-            // ■ 「checkRegularTime」メソッドによって発生する例外に対して、例外処理を行ってください。
-            // ・例外処理は「OvertimeException」、「EarlyLeaveException」の各例外別で行ってください。
-            // ・例外時は、該当例外の内容を表示してください。
-            // ・各例外はメッセージを持っています。例外からメッセージを取得し、標準出力してください。
+            // ■ 「WorkingHourRule」クラスの「checkRegularTime」メソッドに変数「workingHours」を渡して呼び出してください。
+            // 「checkRegularTime」メソッドの実行結果に応じて以下の処理を行ってください。
+            // ・正常終了した場合
+            //   ・文字列「定時帰りです。」を標準出力してください。
+            // ・例外「OvertimeException」が発生した場合
+            //   ・例外をキャッチして、その情報を表示してください。
+            //   ・例外からメッセージを取得し、標準出力してください。
+            // ・例外「EarlyLeaveException」が発生した場合
+            //   ・例外「OvertimeException」が発生時と同様の処理を行ってください。
             try {
                 workingHourRule.checkRegularTime(workingHours);
                 System.out.println("定時帰りです。");
--- src/java_advance_01/WorkingHourRule.java
+++ src/java_advance_01/WorkingHourRule.java
@@ -2,15 +2,16 @@ package java_advance_01;
 
 public class WorkingHourRule {
 
-    // checkRegularTimeメソッド
-    // 引数として渡された勤務時間によって、以下の処理を行う。
-    // - 勤務時間が8時間超過の場合：OverTimeExceptionをメッセージと共に送出する。
-    // - 勤務時間が8時間未満の場合：EarlyLeaveExceptionをメッセージと共に送出する。
-    // - 勤務時間が8時間の場合：「定時帰りです。」を標準出力する。
-    // 引数：workingHours 勤務時間（整数）
-    // 返却値：なし
-    // 例外1：NoMoreOvertimeException 勤務時間が正規勤務時間より大きくて、オーバータイムが発生した場合
-    // 例外2：EarlyLeaveException     勤務時間が正規勤務時間より少なくて、早退が発生した場合
+    /**
+     * 引数として渡された勤務時間の値によって、以下の処理を行う。
+     * - 勤務時間が8時間の場合：「定時帰りです。」を標準出力
+     * - 勤務時間が8時間超過の場合：OverTimeExceptionをメッセージと共に送出
+     * - 勤務時間が8時間未満の場合：EarlyLeaveExceptionをメッセージと共に送出
+     *
+     * @param workingHours 勤務悲観（整数）
+     * @throws OvertimeException   勤務時間が正規勤務時間より大きくて、オーバータイムが発生した場合
+     * @throws EarlyLeaveException 勤務時間が正規勤務時間より少なくて、早退が発生した場合
+     */
     public void checkRegularTime(int workingHours) throws OvertimeException, EarlyLeaveException {
         int officeHours = 8;
         if (workingHours > officeHours) {
