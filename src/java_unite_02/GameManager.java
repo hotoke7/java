51363c90c40d0bfc9310eb47d75ace236e18d104
youngjae.kwon <youngjae.kwon@accenture.com>
2019-04-01 19:17:44 +0900

    Java一貫② じゃんけんゲーム②

--- src/java_unite_02/GameManager.java
+++ src/java_unite_02/GameManager.java
@@ -2,27 +2,37 @@ package java_unite_02;
 
 public class GameManager {
     public static void main(String[] args) {
-        // Lesson Java一貫② じゃんけんゲーム②
-        // 【問題】3回2勝制のじゃんけんゲームを作成してください。
-        // Java一貫① じゃんけんゲーム①のコードを適切に利用してください。
-
-        // ■ ユーザーを示す、Playerのインスタンスを作成してください。
         Player you = new Player();
-
-        // ■ コンピューターを示す、Playerのインスタンスを作成してください。
         Player computer = new Player();
-
-        // ■ ゲームの管理を行う、GameManagerのインスタンスを作成してください。
         RockPaperScissors rockPaperScissors = new RockPaperScissors();
 
-        // ■ 以下を標準出力してください。
-        // じゃんけんゲームを始めます。
+        int gameCount = 1;
         System.out.println("じゃんけんゲームを始めます。");
 
-        // ■ ユーザーかコンピューター、どちらかが2勝するまでゲームを繰り返してください。
-        //    まだ決着がつかなかった場合、カウントを増やしてください。
-        while (!rockPaperScissors.play(you, computer)) {
-            rockPaperScissors.nextRound();
+        while (true) {
+            System.out.println(gameCount + "回戦");
+            rockPaperScissors.play(you, computer);
+            boolean isFinished = checkWinCount(you, computer);
+
+            if (isFinished) {
+                break;
+            }
+            gameCount++;
         }
+    }
+
+    public static boolean checkWinCount(Player you, Player computer) {
+        int yourWinCount = you.getWinCount();
+        int computersWinCount = computer.getWinCount();
+
+        System.out.println("======================================");
+        if (yourWinCount == 2) {
+            System.out.println(yourWinCount + "：" + computersWinCount + "で勝ちました！");
+            return true;
+        } else if (computersWinCount == 2) {
+            System.out.println(yourWinCount + "：" + computersWinCount + "で負けました…");
+            return true;
+        }
+        return false;
     }
 }
--- src/java_unite_02/RockPaperScissors.java
+++ src/java_unite_02/RockPaperScissors.java
@@ -4,86 +4,49 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
-
     static Random random = new Random();
     static Scanner scan = new Scanner(System.in);
 
-    int gameCount;
-
-    public RockPaperScissors() {
-        gameCount = 1;
-    }
-
-    public void nextRound() {
-        gameCount++;
-    }
-
-    public boolean play(Player you, Player computer) {
-        int yourChoice;
-        int computersChoice;
-
-        // ■ サイズが３の文字列の配列を宣言し、「グー」「パー」「チョキ」の文字列を配列に格納してください。
+    // Lesson Java一貫② じゃんけんゲーム②
+    // 【問題】以下のソースコードをもとに、playメソッドを完成してください。
+    public void play(Player you, Player computer) {
+        // サイズが３の文字列の配列を宣言し、「グー」「パー」「チョキ」の文字列を配列に格納します。
         String[] rps = new String[3];
         rps[0] = "グー";
         rps[1] = "パー";
         rps[2] = "チョキ";
 
-        // ■ 以下の文字列を標準出力してください。
-        // じゃんけんゲームを始めます。
-        // 以下の選択肢の中で一つを選んでください。
-        System.out.println(gameCount + "回戦");
         System.out.println("以下の選択肢の中で一つを選んでください。");
 
-        // 配列を繰り返し、ユーザーが選択可能な値を出力してください。
-        // 出力形式
-        //   配列のインデックス：配列の値
-        // 出力結果
-        //   0  : グー
-        //   1  : パー
-        //   2  : チョキ
+        // 配列を繰り返し、配列の値を標準出力します。
         for (int i = 0; i < rps.length; i++) {
             System.out.println(i + "：" + rps[i]);
         }
 
-        //=======================================================================================
-        // ユーザーとコンピューターの選択値を変数に格納する
-        //=======================================================================================
         // コンピュータが出す手を決めます。
-        // ■ 以下のソースコードは配列の長さ分の整数をランダムで返却します。
-        //    ソースコードの返却値を、コンピューターの選択を示す変数に格納してください。
-        computersChoice = random.nextInt(rps.length);
+        int computersChoice = random.nextInt(rps.length);
 
-        // 標準入力の為のガイドメッセージを出力します。
-        // ■ 以下の文字列を標準出力してください。
-        // 番号を入力してください：
         System.out.println("番号を入力してください：");
 
         // ユーザーが出す手を決めます。
-        // ■ 以下のソースコードは、プログラム実行時にユーザーが画面上で入力した数値を取得できます。
-        //    ソースコードの返却値を、ユーザーの選択を示す変数に格納してください。
-        yourChoice = scan.nextInt();
+        int yourChoice = scan.nextInt();
 
-        //=======================================================================================
-
-        // ■ ユーザーとコンピュータのじゃんけんの手を、以下の形式で出力してください。
-        // 出力形式
-        //   あなたの選択：じゃんけんの配列にユーザーの選択値を代入
-        //   コンピューターの選択：じゃんけんの配列にコンピューターの選択値を代入
-        // 出力結果（例）：
-        //   あなたの選択 : グー
-        //   コンピューターの選択 : パー
         System.out.println("あなたの選択 : " + rps[yourChoice]);
         System.out.println("コンピュータの選択 : " + rps[computersChoice]);
 
-        // ■ 条件分岐を利用し、じゃんけんの結果を出力してください。
-        // ユーザーとコンピュータの選択値から、勝ち/負け/あいこを決めて標準出力してください。
-        // 出力する文字列：
-        //   勝ちました！
-        //   あいこです。
-        //   負けました…
+        // ■ 以下のコードをもとに、じゃんけんの結果によってプレイヤーインスタンスの値を変更してください。
+        // 機能詳細：
+        //   ユーザーが勝った場合：
+        //     動作：以下の内容を標準出力及びユーザーの勝利回数カウントを＋1とする。
+        //     出力内容：勝ちました！
+        //   コンピューターが勝った場合：
+        //     動作：以下の内容を標準出力及びコンピューターの勝利回数カウントを＋1とする。
+        //     出力内容：負けました…
+        //   無勝負の場合：
+        //     動作：以下の内容を標準出力する。
+        //     出力内容：あいこです。
         if (yourChoice == computersChoice) {
             System.out.println("あいこです。");
-            return false;
         } else {
             if (yourChoice == 0) {
                 if (computersChoice == 1) {
@@ -110,22 +73,6 @@ public class RockPaperScissors {
                     you.win();
                 }
             }
-            return checkWinCount(you, computer);
         }
-    }
-
-    public boolean checkWinCount(Player you, Player computer) {
-        int yourWinCount = you.getWinCount();
-        int computersWinCount = computer.getWinCount();
-
-        System.out.println("======================================");
-        if (yourWinCount == 2) {
-            System.out.println(yourWinCount + "：" + computersWinCount + "で勝ちました！");
-            return true;
-        } else if (computersWinCount == 2) {
-            System.out.println(yourWinCount + "：" + computersWinCount + "で負けました…");
-            return true;
-        }
-        return false;
     }
 }
