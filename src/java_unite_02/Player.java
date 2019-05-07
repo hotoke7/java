682c43e7064791811f8b7fcdebbd7312695ae017
hotoke7 <kazunori.hotta@rakuten.com>
2019-05-07 15:56:00 +0900

    activity

---
+++ src/java_unite_02/Player.java
@@ -0,0 +1,17 @@
+package java_unite_02;
+
+public class Player {
+    private int winCount;
+
+    public Player (){
+        this.winCount = winCount;
+        winCount = 0;
+    }
+
+    public void Player(){
+        int i;
+        for (i = 0; i < 3; i++){
+
+        }
+    }
+}
